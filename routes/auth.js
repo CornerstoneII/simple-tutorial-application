5fab1b8cbafc53e42a05bc9b0f89e65edd07b75f
CornerstoneII <seunmyles@gmail.com>
2020-05-14 02:01:21 +0100

    user-validation

--- routes/auth.js
+++ routes/auth.js
@@ -13,6 +13,11 @@ router.post("/registertutor", async (req, res) => {
   const { error } = registerValidation(req.body);
   if (error) return res.status(400).send(error.details[0].message);
 
+  // Check if Admin (email) already exist in DB
+  const emailExist = await Admin.findOne({ email: req.body.email });
+  if (emailExist) return res.status(400).send("Email already exists");
+
+  // Creating a Tutor / Admin
   const admin = new Admin({
     firstname: req.body.firstname,
     lastname: req.body.lastname,
@@ -33,6 +38,11 @@ router.post("/registerstudent", async (req, res) => {
   const { error } = registerValidation(req.body);
   if (error) return res.status(400).send(error.details[0].message);
 
+  // Check if user(email) already exist in DB
+  const emailExist = await User.findOne({ email: req.body.email });
+  if (emailExist) return res.status(400).send("Email already exists");
+
+  // Creating a User / Student
   const user = new User({
     firstname: req.body.firstname,
     lastname: req.body.lastname,
